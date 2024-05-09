e13715202ec237e9cbb6c19919173db2ff7bd426
mouadedd <mdx.edd@gmail.com>
2024-05-09 14:12:02 +0000

    fixed an uppercase

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -2,13 +2,12 @@
 
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
-    user_id INT
-)
+    user_id INT)
 BEGIN
     DECLARE weight_avg_score FLOAT;
     SET weight_avg_score = (SELECT SUM(score * weight) / SUM(weight)
                         FROM users
-                        JOIN corrections ON users.id=Corrections.user_id
+                        JOIN corrections ON users.id=corrections.user_id
                         JOIN projects ON corrections.project_id=projects.id
                         WHERE users.id=user_id);
     UPDATE users SET average_score = weight_avg_score WHERE id=user_id;
