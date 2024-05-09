f8a5e3b8c95fa0936701bca38c46fb4a49c13166
mouadedd <mdx.edd@gmail.com>
2024-05-09 13:44:30 +0000

    specified the output of the function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -3,9 +3,9 @@
 DELIMITER
 $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
 BEGIN
-        IF (b = 0)
-                THEN
+        IF (b = 0) THEN
                 RETURN (0);
         ELSE
                 RETURN (a / b);
