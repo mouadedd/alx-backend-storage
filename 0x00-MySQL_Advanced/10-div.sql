ded890eb8288e2b18316e81b1eec358e64865bc7
mouadedd <mdx.edd@gmail.com>
2024-05-09 13:45:56 +0000

    specified the output of the function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,5 +1,4 @@
 -- creates a function SafeDiv that divides (and returns) the first by the second number
--- or returns 0 if the second number is equal to 0.
 DELIMITER
 $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
